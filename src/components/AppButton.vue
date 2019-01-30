d71e2ce70a866af3f0bb48897976d44d00992958
dev-iskan <successfuliskan@gmail.com>
2019-01-30 21:13:56 +0500

    adding themes and sizes alongside to binding  attributes

---
+++ src/components/AppButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <router-link 
+      class="btn" 
+      :to="to"
+      :class="[
+        themes[theme],
+        sizes[size]
+      ]"
+    >
+      Sign In
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+        return {
+          themes: {
+            primary: 'btn-primary',
+            danger: 'btn-danger'
+          },
+          sizes: {
+            normal: '',
+            large: 'btn-lg'
+          }
+        }
+      },
+    props: {
+      theme: {
+        required: false,
+        type: String,
+        default: 'primary'
+      },
+      size: {
+        required: false,
+        type: String,
+        default: 'normal'
+      },
+      to: {
+        required: false,
+        default () {
+          return {
+            name: 'home'
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
